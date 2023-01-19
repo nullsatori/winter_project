7ea2a18eee0b4d625ef4326c1adf526836baf4f9
Kriezdd <blagushin_nikita@mail.ru>
2023-01-19 18:49:58 +0300

    first commit (1 empty component, scss file)

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="Header">
+            <h1>Kinomap</h1>
+        </div>
+    );
+};
+
+export default Header;
