45e6f4b4d039252ff305925fbc4c0d16b8d1da85
satori <nullsatori@gmail.com>
2023-01-28 10:02:44 +0300

    refactor

--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./app";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Корень React приложения
+const root = createRoot(document.getElementById("root"));
+
 root.render(
-    <App />,
+    <BrowserRouter>
+        <App />
+    </BrowserRouter>
 );
